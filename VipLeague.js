7de9fd8dd83ef482a69ee405921e60fc51d4c5e0
Beej <6301228+Beej126@users.noreply.github.com>
2023-12-29 16:47:26 -0800

    js filter and page script tweaks to keep up with VipLeague... nice how easy that is now =)

--- VipLeague.js
+++ VipLeague.js
@@ -16,35 +16,75 @@
 //     return result;
 // } 
 
-//override the eval function to remove the debugger statements that are in the code
-//these forced breaks are intentional blocks put in to prevent inspecting the running elements and code
-const origeval = window.eval;
-window.eval = txt => origeval(txt?.replace(/debugger/gi, ""));
+//override the native browser eval function to remove "debugger" statements
+//these are intended to inhibit inspecting the running elements and code
+const origEval = window.eval;
+window.eval = txt => origEval(txt?.replace(/debugger/gi, ""));
+
+const textMatch = text => node => [text].flat().some(str => node.textContent.includes(str));
+
+const removeNodes = (selector, nodePredicate, parentPredicate) => {
+    let nodes = Array.from(document.querySelectorAll(selector));
+    if (parentPredicate) {
+        // with walking up parents, the assumption is the selector is something generic like "div" and the nodePredicate is looking for text
+        // inconveniently for us here, node.textContent matches the upper most parent where that text could be nested several layers down
+        // so we need to use findLast to start with the lowest level node that matches and work up from there
+        let found = nodes.findLast(node => nodePredicate(node));
+        while (found?.parentNode && parentPredicate(found.parentNode)) found = found.parentNode;
+        found?.remove();
+    }
+    else {
+        nodes.forEach(node => (!nodePredicate || nodePredicate(node)) && node.remove());
+    }
+
+}
 
 window.onload = () => {
-    let mediaClicked;
+    let onlyOnce = true;
+    let onlyTwice = true;
 
     setInterval(() => {
 
-        let deleteMe = Array.from(document.querySelectorAll("div")).findLast(el => el.textContent.includes("banner will go away"));
-        while (deleteMe?.parentNode && getComputedStyle(deleteMe.parentNode).position === "absolute") deleteMe = deleteMe.parentNode;
-        deleteMe?.remove();
+        removeNodes("div", textMatch("banner will go away"), parentNode => getComputedStyle(parentNode).position === "absolute");
 
         // kill all iframes!! except the main video player
-        Array.from(document.querySelectorAll("iframe")).forEach(iframe => iframe.title !== "embed player" && iframe.remove());
+        removeNodes("iframe", node => node.title !== "embed player");
 
         //remove the entire left and right side bar areas where the ads are
-        document.querySelectorAll(".col-12.col-xl-3, .col-12.col-lg-3").forEach(n => n.remove());
+        removeNodes(".col-12.col-xl-3, .col-12.col-lg-3");
 
         //remove buttons that link out to pointless garbage
-        Array.from(document.querySelectorAll("button")).findLast(el => ["Watch Live Stream", "Stream Here Now", "HD Live Stream"].some(str => el.textContent.includes(str)))?.remove();
+        removeNodes("button", textMatch(["Live Stream", "Stream Here Now"]));
 
-        //the media player responds to this programmatic clicks in the debugger console but for some reason the timing is off when it executes here
-        document.querySelector(".jw-state-idle .jw-media")?.click();
-        if (!mediaClicked) {
-            mediaClicked = document.querySelector(".jw-state-paused .jw-media");
-            mediaClicked?.click();
-        }
+        //haven't been able to get media player to automatically start 
+        //after trying everything, pretty sure this is intentionally disabled within the jwplayer api itself
+        //https://github.com/jwplayer/jw-showcase/issues/179
 
-    }, 500);
-}
+        //this first click gets rid of a goofy overlay or something like that in the way 
+        // document.querySelector(".jw-state-idle .jw-media")?.click();
+
+        // if (typeof jwplayer !== "undefined") {
+        //     const jwid = document.querySelector(".jwplayer")?.id;
+        //     // if (jwplayer && ["idle", "paused"].includes(jwplayer().getState())) {
+
+        //     if (!onlyOnce && !onlyTwice && jwid && ["idle", "paused"].includes(jwplayer(jwid).getState())) debugger;
+
+        //     if (!onlyOnce && onlyTwice && jwid && ["idle", "paused"].includes(jwplayer(jwid).getState())) {
+        //         onlyTwice = false;
+        //         jwplayer(jwid).play();
+        //     }
+
+        //     const config = jwplayer(jwid)?.getConfig()?.setupConfig;
+        //     if (onlyOnce && config) {
+        //         onlyOnce = false;
+        //         debugger;
+        //         jwplayer(jwid).remove();
+        //         jwplayer(jwid).setup({ ...config, autostart: true });
+        //         jwplayer(jwid).on('ready', function(e) {
+        //             this.play();
+        //         });
+        //     }
+
+        // }
+    }, 100);
+};
